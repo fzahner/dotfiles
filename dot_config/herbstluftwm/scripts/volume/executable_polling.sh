7abc9da29ae6dd9edb61fa18b2e855b1dea388b9
Fabio Zahner <zahnerfabio@gmail.com>
2025-10-20 16:39:21 +0200

    made herbstluft config more universal

--- dot_config/herbstluftwm/scripts/volume/executable_polling.sh
+++ dot_config/herbstluftwm/scripts/volume/executable_polling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 amixer_output=$(amixer sget 'Master')
-volume=$(amixer sget 'Master' | awk -F'[\[\]]' '/%/ { gsub("%", "", $2); print $2; exit }')
+volume=$(amixer sget Master | awk -F'[][]' '/%/ { gsub("%", "", $2); print $2; exit }')
 
 timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 echo "[$timestamp] Audio Volume: $volume%"
