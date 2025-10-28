0017243eb073c7673183b87c51d9a752f50b9557
Fabio Zahner <zahnerfabio@gmail.com>
2025-10-28 09:14:17 +0100

    added autorandr

---
+++ dot_config/autorandr/xrandr_commands.sh
@@ -0,0 +1,12 @@
+
+# xrandr --dpi 118
+# xrandr --output eDP-1 --mode 2560x1440 --pos 440x1440 --primary
+
+# OST Widescreen setup
+# xrandr --output DP-1 --mode 3440x1440 --pos 0x0
+
+# Home Setup
+# xrandr --output DP-2 --scale 1.25x1.25 --mode 1920x1080 --pos 0x0
+
+# OST SA Room
+# xrandr --output DP-1  --mode 1920x1080 --pos 0x0
