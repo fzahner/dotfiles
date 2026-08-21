4dcdfbc39724a4d5a4e95d2d47b371be1aab9fd1
Fabio Zahner <zahnerfabio@gmail.com>
2026-08-21 15:27:38 +0200

    dynamic clone-term command

--- dot_config/fish/custom_functions/clone-term.fish
+++ dot_config/fish/custom_functions/clone-term.fish
@@ -5,6 +5,10 @@ function clone-term
     if test -z "$count"; set count 1; end
     set cwd (pwd)
     for i in (seq $count)
-        hyprctl dispatch 'hl.dsp.exec_cmd("kitty -d '"$cwd"'")'
+        if set -q HYPRLAND_INSTANCE_SIGNATURE; or test "$XDG_CURRENT_DESKTOP" = "Hyprland"
+            hyprctl dispatch 'hl.dsp.exec_cmd("kitty -d '"$cwd"'")'
+        else if test "$DESKTOP_SESSION" = "herbstluftwm"; or command -q herbstclient
+            herbstclient spawn kitty $cwd
+        end
     end
 end
