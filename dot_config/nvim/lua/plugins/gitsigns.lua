63bab6bdc29b32a7c8b47642ceb1cb7e9b5740a5
Fabio Zahner <zahnerfabio@gmail.com>
2026-09-22 20:59:42 +0200

    nvim: next/prev change with inline hunk preview

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -5,5 +5,39 @@ return {
 		require("gitsigns").setup({
 			current_line_blame = false,
 		})
+		local gitsigns = require("gitsigns")
+		local timer = vim.uv.new_timer()
+
+		-- Jump to next Git change with inline preview
+		vim.keymap.set("n", "]h", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "]c", bang = true })
+			else
+				gitsigns.nav_hunk("next")
+				timer:start(
+					100,
+					0,
+					vim.schedule_wrap(function()
+						gitsigns.preview_hunk_inline()
+					end)
+				)
+			end
+		end, { desc = "Next Git change with inline preview" })
+
+		-- Jump to previous Git change with inline preview
+		vim.keymap.set("n", "[h", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "[c", bang = true })
+			else
+				gitsigns.nav_hunk("prev")
+				timer:start(
+					100,
+					0,
+					vim.schedule_wrap(function()
+						gitsigns.preview_hunk_inline()
+					end)
+				)
+			end
+		end, { desc = "Previous Git change with inline preview" })
 	end,
 }
